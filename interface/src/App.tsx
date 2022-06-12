10585cb27d19f56c936e56e74d86f5bcede83f29
Corey Matthew Capetillo <corey.capetillo@gmail.com>
2022-06-12 10:10:28 -0400

    Component Refactor Overhaul: Part 1 (Cleanup) (#16)
    * Use grid for Header and App
    * Simplify gallery logic
    * Remove extra conditional logic
    * Add Submit button, non-function"
    * Add Form Context and Provider
    * Simplify form inputs, use context

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -3,83 +3,30 @@ import React, { FC, useState } from 'react';
 import {
   Card,
   CardContent,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  MenuItem,
-  Select,
   Typography,
-  createStyles,
+  Container,
+  Grid,
+  CardActions,
+  Button,
 } from '@material-ui/core';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 
 import { callDalleService } from 'api/backend_api';
 import BackendUrlInput from 'components/BackendUrlInput';
 import GeneratedImageList from 'components/GeneratedImageList';
+import Header from 'components/Header';
+import ImagesPerQuerySelect from 'components/ImagesPerQuerySelect';
 import LoadingSpinner from 'components/LoadingSpinner';
 import TextPromptInput from 'components/TextPromptInput';
 
-const useStyles = () =>
-  createStyles({
-    root: {
-      display: 'flex',
-      width: '100%',
-      flexDirection: 'column',
-      margin: '60px 0px 60px 0px',
-      alignItems: 'center',
-      textAlign: 'center',
-    },
-    title: {
-      marginBottom: '20px',
-    },
-    playgroundSection: {
-      display: 'flex',
-      flex: 1,
-      width: '100%',
-      alignItems: 'flex-start',
-      justifyContent: 'center',
-      marginTop: '20px',
-    },
-    settingsSection: {
-      display: 'flex',
-      flexDirection: 'column',
-      padding: '1em',
-      maxWidth: '300px',
-    },
-    searchQueryCard: {
-      marginBottom: '20px',
-    },
-    imagesPerQueryControl: {
-      marginTop: '20px',
-    },
-    formControl: {
-      margin: '20px',
-      minWidth: 120,
-    },
-    gallery: {
-      display: 'flex',
-      flex: '1',
-      maxWidth: '50%',
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'flex-start',
-      padding: '1rem',
-    },
-  });
-
-type Props = WithStyles<typeof useStyles>;
-
-const App: FC<Props> = ({ classes }) => {
-  const [backendUrl, setBackendUrl] = useState('');
+const App: FC = () => {
+  const [backendUrl] = useState('');
   const [isFetchingImgs, setIsFetchingImgs] = useState(false);
-  const [isCheckingBackendEndpoint, setIsCheckingBackendEndpoint] = useState(false);
-  const [isValidBackendEndpoint, setIsValidBackendEndpoint] = useState(true);
+  const [isValidBackendEndpoint] = useState(true);
   const [generatedImages, setGeneratedImages] = useState([]);
   const [apiError, setApiError] = useState('');
-  const [imagesPerQuery, setImagesPerQuery] = useState(2);
+  const [imagesPerQuery] = useState(2);
   const [queryTime, setQueryTime] = useState(0);
 
-  const imagesPerQueryOptions = 10;
   const validBackendUrl = isValidBackendEndpoint && backendUrl;
 
   function enterPressedCallback(promptText: string) {
@@ -107,77 +54,34 @@ const App: FC<Props> = ({ classes }) => {
       });
   }
 
-  function getGalleryContent() {
-    if (apiError) {
-      return (
-        <Typography variant="h5" color="error">
-          {apiError}
-        </Typography>
-      );
-    }
-
-    if (isFetchingImgs) {
-      return <LoadingSpinner isLoading={isFetchingImgs} />;
-    }
-
-    return <GeneratedImageList generatedImages={generatedImages} />;
-  }
-
   return (
-    <div className={classes.root}>
-      <div className={classes.title}>
-        <Typography variant="h3">
-          DALL-E Playground{' '}
-          <span role="img" aria-label="sparks-emoji">
-            ✨
-          </span>
-        </Typography>
-      </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+        {!validBackendUrl && (
+          <Grid item xs={12}>
+            <Typography variant="body1" color="textSecondary">
+              Put your DALL-E backend URL to start
+            </Typography>
+          </Grid>
+        )}
 
-      {!validBackendUrl && (
-        <div>
-          <Typography variant="body1" color="textSecondary">
-            Put your DALL-E backend URL to start
-          </Typography>
-        </div>
-      )}
-
-      <div className={classes.playgroundSection}>
-        <div className={classes.settingsSection}>
-          <Card className={classes.searchQueryCard}>
+        <Grid item xs={4}>
+          <Card>
             <CardContent>
-              <BackendUrlInput
-                setBackendValidUrl={setBackendUrl}
-                isValidBackendEndpoint={isValidBackendEndpoint}
-                setIsValidBackendEndpoint={setIsValidBackendEndpoint}
-                setIsCheckingBackendEndpoint={setIsCheckingBackendEndpoint}
-                isCheckingBackendEndpoint={isCheckingBackendEndpoint}
-                disabled={isFetchingImgs}
-              />
+              <BackendUrlInput isDisabled={isFetchingImgs} />
               <TextPromptInput
                 enterPressedCallback={enterPressedCallback}
                 disabled={isFetchingImgs || !validBackendUrl}
               />
-
-              <FormControl className={classes.imagesPerQueryControl} variant="outlined">
-                <InputLabel id="images-per-query-label">Images to generate</InputLabel>
-                <Select
-                  labelId="images-per-query-label"
-                  label="Images per query"
-                  value={imagesPerQuery}
-                  disabled={isFetchingImgs}
-                  onChange={(event) => setImagesPerQuery(parseInt(event.target.value as string))}
-                >
-                  {Array.from(Array(imagesPerQueryOptions).keys()).map((num) => {
-                    return (
-                      <MenuItem key={num + 1} value={num + 1}>
-                        {num + 1}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-                <FormHelperText>More images = slower query</FormHelperText>
-              </FormControl>
+              <ImagesPerQuerySelect isDisabled={isFetchingImgs} />
+              <CardActions>
+                <Button variant="contained" color="primary" fullWidth>
+                  Submit
+                </Button>
+              </CardActions>
             </CardContent>
           </Card>
           {queryTime !== 0 && (
@@ -185,13 +89,21 @@ const App: FC<Props> = ({ classes }) => {
               Query execution time: {queryTime} sec
             </Typography>
           )}
-        </div>
-        {(generatedImages.length > 0 || apiError || isFetchingImgs) && (
-          <div className={classes.gallery}>{getGalleryContent()}</div>
-        )}
-      </div>
-    </div>
+        </Grid>
+        <Grid item xs={8}>
+          {!!apiError && (
+            <Typography variant="h5" color="error">
+              {apiError}
+            </Typography>
+          )}
+          {isFetchingImgs && <LoadingSpinner searchTerm={''} isLoading={isFetchingImgs} />}
+          {generatedImages.length > 0 && <GeneratedImageList generatedImages={generatedImages} />}
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
-export default withStyles(useStyles)(App);
+App.displayName = 'App';
+
+export default App;
